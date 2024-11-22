fc3025d8f8bf918f6158e5491bb94ef50b578d3b
Jordon Leach <jordonleach@gmail.com>
2024-11-22 13:49:53 -0500

    Add lawndon-pi to tests workflow - lint UI

--- pi/ui/vite.config.ts
+++ pi/ui/vite.config.ts
@@ -1,8 +1,8 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,14 +10,6 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000',
-    },
-  },
-})
+  resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
+  server:  { proxy: { '/api': 'http://localhost:5000' } },
+});
