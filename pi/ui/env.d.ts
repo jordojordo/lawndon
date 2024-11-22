c7da0a7382bb3c38345df9579d039871c37eb9da
Jordon Leach <jordonleach@gmail.com>
2024-11-22 14:01:30 -0500

    Add eslint to server - update deps - fix ui build error

--- pi/ui/env.d.ts
+++ pi/ui/env.d.ts
@@ -1,1 +1,10 @@
 // / <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly BASE_URL: string;
+  readonly PROD: boolean
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
