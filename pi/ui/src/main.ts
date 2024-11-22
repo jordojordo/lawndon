215c7a510113f92cf058a8795aae0242f4499fcd
Jordon Leach <jordonleach@gmail.com>
2024-11-22 13:49:53 -0500

    Add lawndon-pi to tests workflow - lint UI

--- pi/ui/src/main.ts
+++ pi/ui/src/main.ts
@@ -1,21 +1,19 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import { io, Socket } from 'socket.io-client'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { io, Socket } from 'socket.io-client';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
+const app = createApp(App);
 
-const socket: Socket = io('http://0.0.0.0:5000', {
-  transports: ['websocket'],
-})
+const socket: Socket = io('http://0.0.0.0:5000', { transports: ['websocket'] });
 
-app.provide('socket', socket)
+app.provide('socket', socket);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
--- pi/ui/src/views/HomeView.vue
+++ pi/ui/src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import UWBVisualization from '../components/UWBVisualization.vue'
+import UWBVisualization from '../components/UWBVisualization.vue';
 </script>
 
 <template>
