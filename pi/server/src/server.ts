4d296e7a0d51aa182c4cccdadcaee7e16c208310
Jordon Leach <jordonleach@gmail.com>
2024-11-22 05:38:55 -0500

    Fix UI static location - start dev loading functionality (#16)

--- pi/server/src/server.ts
+++ pi/server/src/server.ts
@@ -12,7 +12,7 @@ const io = new Server(httpServer, {
 
 const PORT = 5000;
 
-app.use(express.static(path.join(__dirname + '../../ui/index.html')));
+app.use(express.static(path.join(__dirname + '../../ui')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '../../ui/index.html'));
